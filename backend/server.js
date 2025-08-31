8f5700f8bcad2a6e2c795412d4187038e632185d
Priyanshi <2k22.csai.2212134@gmail.com>
2025-08-31 19:09:08 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import uploadRoutes from "./routes/upload.js";
 import path from "path";
+import {fileURLToPath} from "url";
 dotenv.config();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
@@ -28,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-const __dirname = path.resolve();
+
 if(process.env.NODE_ENV === "production") {
 
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
